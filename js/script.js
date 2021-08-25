0d5b8b70f5fa2ce0ce4dacd592a2276e27670716
Carms Ng <contact@carms-ng.com>
2021-08-25 11:58:53 -0400

    [UPD] Animate wave

--- js/script.js
+++ js/script.js
@@ -3,14 +3,19 @@ let xs = []
 for (var i = 9; i < 500; i++) {
   xs.push(i)
 }
-// console.log(xs)
+
+// set time to 0
+let t = 0
 
 function animate() {
-  // console.log('sup')
 
   let points = xs.map(x => {
-    // calculation
-    return [x, x + Math.random() * 20]
+    // electrical line
+    // return [x, x + Math.random() * 20]
+
+    //      offsetY      frequency   wavelength
+    let y = 200 + Math.sin((x + t) / 20) * 50
+    return [x, y]
   })
 
   let path = "M" + points.map(([x, y]) => {
@@ -18,6 +23,9 @@ function animate() {
   }).join (" L")
   // console.log(path)
   document.querySelector("path").setAttribute("d", path)
+
+  // speed
+  t += 1
 
   requestAnimationFrame(animate)
 }
